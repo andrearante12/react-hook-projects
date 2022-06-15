128ddd3f67f62db1fd72370e2748dafbf08d3f06
andrearanteprojects <cactusduuude1@gmail.com>
2022-06-14 18:27:05 -0700

    finished birthdays

--- birthdays-useState/birthdays/src/PeopleData.js
+++ birthdays-useState/birthdays/src/PeopleData.js
@@ -3,6 +3,20 @@ export var PeopleData = [
     id: 1,
     name: "Charlie Brown",
     age: 8,
-    img: url("./static/imgs/charlie-brown.jpg"),
+  },
+  {
+    id: 2,
+    name: "Arthur",
+    age: 8,
+  },
+  {
+    id: 3,
+    name: "Elmo",
+    age: 3,
+  },
+  {
+    id: 4,
+    name: "Jotaro Kujo",
+    age: 17,
   },
 ];
--- birthdays-useState/birthdays/src/components/People.js
+++ birthdays-useState/birthdays/src/components/People.js
@@ -1,22 +1,56 @@
 import React, { useState } from "react";
+import charlie from "../static/imgs/charlie-brown.png";
+import arthur from "../static/imgs/arthur.png";
+import elmo from "../static/imgs/elmo.png";
+import jotaro from "../static/imgs/jotaro.jpeg";
 
 import { PeopleData } from "../PeopleData";
 
 const People = () => {
-  const [birthdays, setBirthdays] = useState(0);
   const [people, setPeople] = useState(PeopleData);
+  const [imgs, setImgs] = useState([charlie, arthur, elmo, jotaro]);
+  const [birthdays, setBirthdays] = useState(people.length);
+
+  const handleClear = () => {
+    setPeople({});
+    setBirthdays(0);
+  };
+
+  const display = () => {
+    if (people.length > 0) {
+      return (
+        <>
+          <h1>{birthdays} Birthdays Today</h1>
+          {people.map((person) => {
+            const { name, age, id } = person;
+
+            return (
+              <div key={id} className="flex-container">
+                <div className="flex-child">
+                  <img src={imgs[id - 1]} alt={name}></img>
+                </div>
+                <div className="flex-child">
+                  <h3 className="name">
+                    <b>{name}</b> <br />{" "}
+                    <span className="age-text">{age} years</span>
+                  </h3>
+                </div>
+              </div>
+            );
+          })}
+
+          <button type="button" className="clear-btn" onClick={handleClear}>
+            Clear All
+          </button>
+        </>
+      );
+    }
+    return <h1>{birthdays} Birthdays Today</h1>;
+  };
+
   return (
-    <section className="container">
-      <h1>{birthdays} Birthdays Today</h1>
-      {people.map((person) => {
-        const { name, age, id, img } = person;
-        return (
-          <div key={id}>
-            <h3>{name}</h3>
-            <img src={img} alt={name}></img>;
-          </div>
-        );
-      })}
+    <section className="container" style={{ fontSize: 25 }}>
+      {display()}
     </section>
   );
 };
